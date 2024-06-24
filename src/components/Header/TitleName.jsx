d2bf1e178d3cd8469f92381c4e26fb5f56742542
RAM <sashatehnik78@gmail.com>
2024-06-24 16:29:37 +0400

    newnewn

--- src/components/Header/TitleName.jsx
+++ src/components/Header/TitleName.jsx
@@ -1,12 +1,24 @@
 import { useTelegram } from "../../hooks/useTelegram"
+import { useState } from "react"
 
 export default function TitleName() {
-  const {user, onSpecialButton} = useTelegram()
+  const {user} = useTelegram()
+  const [typeButton, setType] = useState("Show main button")
+
+  const onSpecialButton = () => {
+    if(!tg.MainButton.isVisible) {
+        tg.MainButton.show()
+        setType("Hide main button")
+    } else {
+        tg.MainButton.hide()
+        setType("Show main button")
+    }
+}
 
   return (
     <>
       <h5>Здравствуйте {useTelegram().user.first_name}</h5>
-      <button onClick={onSpecialButton}>Show main button</button>
+      <button onClick={onSpecialButton}>{typeButton}</button>
     </>
   )
 }
