a1c1b26b5cdbeb673b5850a0ab1937f0c469ecb0
RAM <sashatehnik78@gmail.com>
2024-06-28 20:44:47 +0300

    ww

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -11,7 +11,8 @@ function Card({item, onAdd, onRemove}) {
     }
 
     const removeItem = () => {
-        if(count === 0) {
+        let counter = count - 1
+        if(counter === 0) {
             setCount(0)
             onRemove(item)
         } else {
