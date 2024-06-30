976fc6684873813a45fc81597a2a63234c1d0c42
RAM <sashatehnik78@gmail.com>
2024-06-30 06:35:57 +0300

    hh

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -19,25 +19,28 @@ function Card({item, onAdd, onRemove}) {
 
     return (
         <div className='card'>
-            <span className={`${count !== 0 ? "card_badge" : "card_badge--hidden"}`}>{count}</span>
-            <div className="image__container">
-                <img src={item.image}/>
+            <div className="card_top">
+                <span className={`${count !== 0 ? "card_badge" : "card_badge--hidden"}`}>{count}</span>
+                <div className="image__container">
+                    <img src={item.image}/>
+                </div>
             </div>
-            <div className={"card__title"}>
+
+            <div className="card_bottom">
+                <div className={"card__title"}>
                     {item.title} • ₽<span><b>{item.price}</b></span>
-            </div>
-            <div className="btn-container">
-                {
+                </div>
+                <div className="btn-container">
+                    {
                     count === 0 ? 
                     <Button title={"Добавить"} type={"add"} disable={false} onClick={addItem}/> :
                     <div className='container'>
                         <Button title={"-"} type={"decrease"} disable={false} onClick={removeItem}/>
                         <Button title={"+"} type={"increase"} disable={false} onClick={addItem}/>
                     </div>
-                }
+                    }
+                </div>
             </div>
-
-
 
         </div>
     )
