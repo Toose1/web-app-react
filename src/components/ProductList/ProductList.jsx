d6fa028ee32836b7cc17d3918da7cfab8924ca49
RAM <sashatehnik78@gmail.com>
2024-06-30 04:58:13 +0300

    ww

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -70,7 +70,7 @@ export default function ProductList() {
     
   return (
     <>
-      <div className={"cards"}>
+      <div className="cards">
         {products.map(item => (
           <Card item={item} onAdd={onAdd} onRemove={onRemove}/>
           ))}
