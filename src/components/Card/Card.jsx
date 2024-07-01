ff0dfc9a0d558440029aaa1f15ee5f65446de9df
RAM <sashatehnik78@gmail.com>
2024-07-01 04:09:18 +0300

    ww

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -23,7 +23,7 @@ function Card({item, onAdd, onRemove}) {
             <div className="card_top">
                 <span className={`${count !== 0 ? "card_badge" : "card_badge--hidden"}`}>{count}</span>
                 <div className="image__container">
-                    <ButtonRef item={item} onClick={() => <Button title={"Добавить"} type={"add"} disable={false} onClick={addItem}/>}/>
+                    <ButtonRef item={item} onClick={() => console.log(`go ~${item.title}`)}/>
                 </div>
             </div>
 
