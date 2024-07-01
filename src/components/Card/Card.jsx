4e1808d2a58d2f40db595214ba3222750e5892d8
RAM <sashatehnik78@gmail.com>
2024-07-01 03:19:56 +0300

    qq

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -23,7 +23,7 @@ function Card({item, onAdd, onRemove}) {
             <div className="card_top">
                 <span className={`${count !== 0 ? "card_badge" : "card_badge--hidden"}`}>{count}</span>
                 <div className="image__container">
-                    <ButtonRef item={item} disable={true}/>
+                    <ButtonRef item={item} disable={false}/>
                 </div>
             </div>
 
