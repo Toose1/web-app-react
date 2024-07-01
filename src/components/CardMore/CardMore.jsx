bf39658d9d0ade576a96ee304eb9ae7ae9661e3d
RAM <sashatehnik78@gmail.com>
2024-07-01 19:12:50 +0300

    ww

--- src/components/CardMore/CardMore.jsx
+++ src/components/CardMore/CardMore.jsx
@@ -5,7 +5,7 @@ export default function CardMore({item, onClick}) {
     return (
         <>
             <div className="card__info">
-                <h1>Информация</h1>
+                Информация
             </div>
         </>
     )
