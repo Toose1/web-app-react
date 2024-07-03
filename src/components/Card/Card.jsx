a81ea9e1477200eb7af017f17c4a8d038ac10df1
RAM <sashatehnik78@gmail.com>
2024-07-03 20:09:15 +0300

    ww

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -6,6 +6,12 @@ import CardMore from '../CardMore/CardMore'
 import UniButton from '../UniButton/UniButton'
 import GroupButton from '../GroupButton/GroupButton'
 
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+
 
 function Card({item, onAdd, onRemove}) {
     const [count, setCount] = useState(0)
@@ -33,35 +39,89 @@ function Card({item, onAdd, onRemove}) {
     }
 
     return (
-        <div className='card'>
-            <div className="card_top">
-                <span className={`${count !== 0 ? "card_badge" : "card_badge--hidden"}`}>{count}</span>
-                <div className="image__container">
-                    <ButtonRef item={item} onClick={onActive} />
-                </div>
-            </div>
-
-            <div className={"card__title"}>
-                {item.title} • ₽<span><b>{item.price}</b></span>
-            </div>
+        <>
+            <Card sx={{ maxWidth: 200}}>
+                <CardActionArea>
+                    <CardMedia
+                        component="img"
+                        height="180"
+                        image={item.image}
+                        alt="green iguana"
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h6" component="div">
+                            {item.title}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            {item.description}
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
             <div className="btn-container">
                 {count === 0 ?
-                    <UniButton title={"ADD"} onClick={addItem} sx={{borderRadius: "10px"}}/> :
-                    <GroupButton onRemove={removeItem} onAdd={addItem} />
-                    // <div className='container'>
-                    //     <UniButton title={"-"} onClick={removeItem} sx={{width: "20px"}}/>
-                    //     <UniButton title={"+"} onClick={addItem} sx={{width: "20px"}}/>
-                    // </div>
+                <UniButton title={"ADD"} onClick={addItem} sx={{borderRadius: "10px"}}/> :
+                <GroupButton onRemove={removeItem} onAdd={addItem} />
                 }
             </div>
-
-            {moreInfo && (
-                <div className="card-more-overlay">
-                    <CardMore item={item} onClose={onDisactive} />
-                </div>
-            )}
-        </div>
+        </>
     )
 }
 
 export default Card
+
+
+
+
+
+
+// const [count, setCount] = useState(0)
+// const [moreInfo, setMoreInfo] = useState(false)
+
+// const addItem = () => {
+//     setCount(count+1)
+//     onAdd(item)
+// }
+
+// const removeItem = () => {
+
+//     setCount(count-1)
+//     onRemove(item)
+    
+// }
+
+// const onActive = () => {
+//     setMoreInfo(true)
+//     console.log(`${item.title}`)
+// }
+
+// const onDisactive = () => {
+//     setMoreInfo(false)
+// }
+
+// return (
+//     <div className='card'>
+//         <div className="card_top">
+//             <span className={`${count !== 0 ? "card_badge" : "card_badge--hidden"}`}>{count}</span>
+//             <div className="image__container">
+//                 <ButtonRef item={item} onClick={onActive} />
+//             </div>
+//         </div>
+
+//         <div className={"card__title"}>
+//             {item.title} • ₽<span><b>{item.price}</b></span>
+//         </div>
+//         <div className="btn-container">
+//             {count === 0 ?
+//                 <UniButton title={"ADD"} onClick={addItem} sx={{borderRadius: "10px"}}/> :
+//                 <GroupButton onRemove={removeItem} onAdd={addItem} />
+//             }
+//         </div>
+
+//         {moreInfo && (
+//             <div className="card-more-overlay">
+//                 <CardMore item={item} onClose={onDisactive} />
+//             </div>
+//         )}
+//     </div>
+// )
